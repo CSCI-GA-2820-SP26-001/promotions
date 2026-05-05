650ef9ebaabe3b38549bddcef375af89dd37087a
ouch-ouch <burkules77@gmail.com>
2026-05-05 20:17:27 +0000

    fixes

--- tests/factories.py
+++ tests/factories.py
@@ -7,7 +7,7 @@ import factory
 from service.models import Promotion
 
 
-class PromotionFactory(factory.Factory):
+class PromotionFactory(factory.Factory):  # pylint: disable=too-few-public-methods
     """Creates fake promotions that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
